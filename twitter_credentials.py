58de96717f813c616eed64d15324ed9574c13de9
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-04-25 21:33:34 -0500

    First Submission of final project

---
+++ twitter_credentials.py
@@ -0,0 +1,5 @@
+#Variables que contienen las credenciales de usuario para acceder al API de Twitter.
+ACCESS_TOKEN = "2262123079-roRWpakcoL4uZmZPYId61ycZSteswTs75aGXnhn"
+ACCESS_TOKEN_SECRET = "lisn34Wi6reqKHgLaxGt0NN8y9WZeFlUZXFVtRBeeChHQ"
+CONSUMER_KEY = "LHshE9H4lilLGB4z3xHgCPa0C"
+CONSUMER_SECRET = "rMmT3EGmrf9AdxGrLl5dUt63SnL5QMeGcsYJlxykUJgzQtSAn3"
